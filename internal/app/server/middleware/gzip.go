673e8f457955ae225906967ab8a6c17e41cb42dd
nayakunin <nayakunin99@gmail.com>
2023-03-06 14:06:49 +0100

    Iter 8 Add gzip middleware

---
+++ internal/app/server/middleware/gzip.go
@@ -0,0 +1,46 @@
+package middleware
+
+import (
+	"compress/gzip"
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+type gzipWriter struct {
+	gin.ResponseWriter
+	writer *gzip.Writer
+}
+
+func (w gzipWriter) Write(b []byte) (int, error) {
+	return w.writer.Write(b)
+}
+
+func (w gzipWriter) WriteString(s string) (int, error) {
+	return w.writer.Write([]byte(s))
+}
+
+func Gzip() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		if c.Request.Header.Get("Accept-Encoding") != "gzip" {
+			c.Next()
+			return
+		}
+
+		gz, err := gzip.NewWriterLevel(c.Writer, gzip.BestSpeed)
+		if err != nil {
+			log.Fatal(c.AbortWithError(500, err))
+			return
+		}
+
+		defer func(gz *gzip.Writer) {
+			err := gz.Close()
+			if err != nil {
+				log.Fatal(c.AbortWithError(500, err))
+			}
+		}(gz)
+
+		c.Writer.Header().Set("Content-Encoding", "gzip")
+		c.Writer = &gzipWriter{c.Writer, gz}
+		c.Next()
+	}
+}
--- internal/app/server/router.go
+++ internal/app/server/router.go
@@ -3,11 +3,13 @@ package server
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nayakunin/shortener/internal/app/handlers"
+	"github.com/nayakunin/shortener/internal/app/server/middleware"
 	"github.com/nayakunin/shortener/internal/app/storage"
 )
 
 func setupRouter(s storage.Storager) *gin.Engine {
 	r := gin.Default()
+	r.Use(middleware.Gzip())
 	r.POST("/", handlers.SaveLinkHandler(s))
 	r.GET("/:id", handlers.GetLinkHandler(s))
 	r.POST("/api/shorten", handlers.ShortenHandler(s))
