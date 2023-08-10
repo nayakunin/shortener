ec7c9c1d77c164f769ebf816358f0afa0ebd6770
nayakunin <nayakunin99@gmail.com>
2023-08-10 11:29:33 +0200

    Iter 19 Add comments

--- internal/app/storage/dbpool.go
+++ internal/app/storage/dbpool.go
@@ -7,10 +7,12 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+// DBPool is an interface for pgxpool.Pool
 type DBPool interface {
 	Acquire(ctx context.Context) (*pgxpool.Conn, error)
 	Begin(ctx context.Context) (pgx.Tx, error)
 	Ping(ctx context.Context) error
 }
 
+// InitDBFunc is a function that is used to initialize database
 type InitDBFunc func(conn *pgxpool.Conn) error
