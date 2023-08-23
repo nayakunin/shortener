1b04377a67c6162fc0e09996747445ea8d181fd2
nayakunin <nayakunin99@gmail.com>
2023-08-23 16:38:19 +0200

    Iter 22 Add comment

--- internal/app/server/middleware/wg.go
+++ internal/app/server/middleware/wg.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// WaitGroup is a middleware that counts the number of active requests.
 func WaitGroup(wg *sync.WaitGroup) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		wg.Add(1)
